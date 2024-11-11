2829772bcaf2192c65ea79e5939509aae3efc0c6
Rahul C <rahul.c@heraizen.com>
2024-11-11 23:30:14 +0530

    first server code

---
+++ src/main/java/savelife/com/SavelifeApplication.java
@@ -0,0 +1,13 @@
+package savelife.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavelifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavelifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/savelife/com/SavelifeApplicationTests.java
@@ -0,0 +1,13 @@
+package savelife.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavelifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
